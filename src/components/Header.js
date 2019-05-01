03c6475afbe076c94f735615b99e05f2dc8e5d29
TheSweed <philip.allanson@gmail.com>
2019-05-01 14:22:20 +0200

    Initial Components Installed Axios, Bootstrap & Redux

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../App.css';
+import axios from 'axios';
+import Modal from 'react-awesome-modal';
+
+class Header extends Component {
+    render() {
+        return (
+            <p>Hello</p>
+        );
+    }
+}
+
+export default Header;
