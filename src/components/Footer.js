cf611c670d8fbeab025428023c95da2ee64a7a10
TheSweed <philip.allanson@gmail.com>
2019-05-18 16:42:32 +0200

    InitialState

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import '../index.css';
 
 const Footer = () => (
     <div className="container">
-        <footer className="footer py-3 bg-dark fixed-bottom">
+        <footer className="py-3 bg-dark fixed-bottom">
             <div className="container">
-                <p className="m-0 text-center text-white">Copyright &copy; the market 2019</p>
+                <p className="m-0 text-center text-white">Copyright &copy; the market 2019 | Sitemap</p>
             </div>
         </footer>
     </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,29 @@
 import { combineReducers } from "redux/es/redux"
+import {CategoryFilters, SET_CATEGORY} from "../actions";
+
+/*
+    Default State Variables:
+    Category:
+    Login:
+    Cart:
+ */
+
+const initialState = {
+    category: CategoryFilters.SHOW_ALL,
+
+}
+
+function the_market(state = initialState, action) {
+    // TODO: Handle actions here
+    switch (action.type) {
+        case SET_CATEGORY:
+            return Object.assign({}, state, {
+                category: action.filter
+            });
+        default:
+            return state
+    }
+}
 
 export default combineReducers({
-
 })
