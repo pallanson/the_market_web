4cbb413dc8b8302a6cb8e096c8f1332bbc51d414
Danfeng Wang <danfeng.trondset@gamil.com>
2019-05-30 23:07:57 +0200

    Account Page Options

--- src/containers/PaymentsPage/index.js
+++ src/containers/PaymentsPage/index.js
@@ -1,12 +1,17 @@
 import {Redirect, Route, Switch} from 'react-router-dom'
-import React from 'react'
+import React, {memo} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../index.css';
 import auth from '../../utils/auth';
+import Actions from '../../actions'
+import { makeSelectIsAuthed, makeSelectError } from '../../selectors'
+import { createStructuredSelector } from 'reselect'
 import AccountSidebar from '../../components/AccountSidebar'
 import AddPaymentForm from "../../components/AddPaymentForm";
+import {connect} from "react-redux";
+import {compose} from "redux";
 
-export default (props) => {
+const PaymentsPage = ({addPaymentOption, location}) => {
     const isAuthed = auth.getToken() != null
 
     return isAuthed ? (
@@ -18,8 +23,27 @@ export default (props) => {
         <Redirect
             to={{
                 pathname: '/',
-                state: {from: props.location},
+                state: {from: location},
             }}
         />
     )
 }
+
+const mapStateToProps = createStructuredSelector({
+    authed: makeSelectIsAuthed(),
+    error: makeSelectError()
+})
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addPaymentOption: (evt, nameOnCard, cardNumber, expiryDate) => {
+            if (evt !== null && evt.preventDefault) evt.preventDefault();
+            dispatch(Actions.addPaymentOption(nameOnCard, cardNumber, expiryDate))
+        }
+    }
+}
+const withConnect = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)
+
+export default compose(withConnect, memo)(PaymentsPage);
